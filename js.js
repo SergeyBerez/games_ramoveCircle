5da6739e08e0df4410d62877ecf45d683e8fbe54
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2019-10-24 13:34:02 +0300

    1

--- js.js
+++ js.js
@@ -1,47 +1,58 @@
-window.addEventListener('load', function(e) {
-  let boxs = document.querySelectorAll('.box');
-  let text = '';
-  setInterval(function() {
-    for (let i = 0; i < boxs.length; i++) {
-      if (boxs[i].classList.contains('border')) {
-        boxs[i].classList.remove('border');
-        boxs[i].textContent = text;
-      }
+let text = '';
+let boxs = $('.box');
+console.log(boxs.length);
+setInterval(function() {
+  let boxs = $('.box');
+  for (let i = 0; i < boxs.length; i++) {
+    if (boxs[i].classList.contains('border')) {
+      boxs[i].classList.remove('border');
+      boxs[i].textContent = text;
     }
-    let random = Math.floor(Math.random() * (boxs.length + 1 - 1));
-    boxs[random].classList.add('border');
-    text = boxs[random].innerText;
-    boxs[random].textContent = 'ПОПАДИ';
-  }, 1000);
-  // ===================================
+  }
+  let random = Math.floor(Math.random() * (boxs.length + 1 - 1));
 
-  let counter = 0;
-  document.addEventListener('click', function(e) {
-    if (e.target.classList.contains('border')) {
-      e.target.style.opacity = 0;
-      counter++;
-      console.log(counter);
-    }
-    if (e.target.classList.contains('overlay')) {
-      window.location.reload();
-    }
-    console.log(e.target);
-    if (counter == boxs.length) {
-      createDiv();
-    }
-  });
-  // ===================================
-  function createDiv() {
-    let divOverlay = document.createElement('div');
-    divOverlay.classList.add('overlay');
-    let divModal = document.createElement('div');
-    divModal.classList.add('modal');
-    divModal.textContent = 'YOU WIN';
-    document.body.insertAdjacentElement('afterbegin', divModal);
-    document.body.insertAdjacentElement('afterbegin', divOverlay);
+  if (boxs.length != 0) {
+    
+    boxs[random].classList.add('border'); //добавить класс может через .[i]
+    text = boxs.eq(random).text(); // здесь получить текст не может нужно медоту eq( i) обратиться
+
+    boxs[random].textContent = 'ПОПАДИ';
+  }
+}, 1000);
+// ===================================
+
+document.addEventListener('click', function(e) {
+  if (e.target.classList.contains('border')) {
+    $(e.target).fadeOut(900);
+    $(e.target).attr('data-elem', 'rem');
+
+    setTimeout(() => {
+      $('[data-elem]').remove();
+    }, 2000);
+  }
+  if (e.target.classList.contains('overlay')) {
+    window.location.reload();
+  }
+
+  if ($('.box').length == 2) {
+    $('.box').remove();
+    createDiv();
   }
 });
+// ===================================
+function createDiv() {
+  let divOverlay = document.createElement('div');
+  divOverlay.classList.add('overlay');
+  let divModal = document.createElement('div');
+  divModal.classList.add('modal');
+  divModal.textContent = 'YOU WIN';
+  document.body.insertAdjacentElement('afterbegin', divModal);
+  document.body.insertAdjacentElement('afterbegin', divOverlay);
+}
+//==========================================
 let h3 = document.querySelector('h3');
 setInterval(function() {
   h3.classList.toggle('shadow');
 }, 500);
+// console.log($('.box').css('display'));
+// console.log(boxs[1].visible == false);
